68b1e89d578a1b07e5cc4ed826b51f316b6d709a
Sahlet <sahlet@yandex.ru>
2016-11-30 08:50:16 +0200

    commit

---
+++ server.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  # By declaring get_my_table as a reactive expression we ensure 
+  # that:
+  #
+  #  1) It is only called when the inputs it depends on changes
+  #  2) The computation and result are shared by all the callers 
+  #	  (it only executes a single time)
+  #
+  get_my_table <- reactive({
+    result <- NULL;
+    if (!is.null(input$file1) && !is.null(input$file1$datapath)){
+      result <- read.table(input$file1$datapath, header=input$header, sep=input$sep);
+      if (!input$header) {
+        colnames(result) <- c('x', 'y');
+      }
+    }
+  });
+  
+  output$contents <- renderTable({
+    return(get_my_table());
+  });
+   
+  output$correlation_field <- renderPlot({
+    my_table <- get_my_table();
+    if (!is.null(my_table)){
+      return(plot(my_table));
+    }
+    return(NULL);
+  })
+  
+  #clear_temp_data(output);
+  
+  return(NULL);
+})
---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+#shinyUI(
+  
+  fluidPage(
+    titlePanel("Random processes Lab1"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        fileInput('file1', 'Choose CSV File',
+                  accept=c('text/csv', 
+                           'text/comma-separated-values,text/plain', 
+                           '.csv')),
+#        tags$hr(),
+        checkboxInput('header', 'Header', FALSE),
+        radioButtons('sep', 'Separator',
+                     c(Space = '',
+                       Comma=',',
+                       Semicolon=';',
+                       Tab='\t'),
+                     '')
+      ),
+      mainPanel(
+        plotOutput('correlation_field'),
+        tableOutput('contents')
+      )
+    )
+      
+  )
+  
+#)
